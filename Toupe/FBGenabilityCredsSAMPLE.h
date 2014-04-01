75e6515213c37e05729c3fb12bf021e43fc3d68c
Emile Baizel <ebaizel@gmail.com>
2014-03-31 23:20:27 -0700

    cleanup

---
+++ Toupe/FBGenabilityCredsSAMPLE.h
@@ -0,0 +1,15 @@
+//
+//  FBGenabilityCredsSAMPLE.h
+//  toupe
+//
+//  Created by emileleon on 3/31/14.
+//  Copyright (c) 2014 Fresh Basil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBGenabilityCredsSAMPLE : NSObject
+
++ (NSString *)genCreds;
+
+@end
---
+++ Toupe/FBGenabilityCredsSAMPLE.m
@@ -0,0 +1,16 @@
+//
+//  FBGenabilityCredsSAMPLE.m
+//  toupe
+//
+//  Created by emileleon on 3/31/14.
+//  Copyright (c) 2014 Fresh Basil. All rights reserved.
+//
+
+#import "FBGenabilityCredsSAMPLE.h"
+
+@implementation FBGenabilityCredsSAMPLE
+
++ (NSString *) genCreds {
+    return @"appId=YOURAPPID&appKey=YOURAPPKEY";
+}
+@end
